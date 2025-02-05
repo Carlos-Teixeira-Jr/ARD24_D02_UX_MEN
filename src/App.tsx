c46522a5313c240c76ebb3fdfe0ceaecda0a4084
Gian Vieceli <gianvieceli@gmail.com>
2025-02-05 00:58:29 -0300

    footer e header end

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import { RegisterProductPage } from "./pages/register";
-import { PrivateRoute } from "./routes/PrivateRoute";
-import { EditProductPage } from "./pages/edit";
-import { UserConfigPage } from "./pages/userConfig";
+// import { Route, Routes } from "react-router-dom";
+// import { RegisterProductPage } from "./pages/register";
+// import { PrivateRoute } from "./routes/PrivateRoute";
+// import { EditProductPage } from "./pages/edit";
+// import { UserConfigPage } from "./pages/userConfig";
 import Header from "./pages/Header";
+import Footer from "./pages/Footer";
 
 function App() {
   return (
     <>
-      <Routes>
-        {/* Rotas publicas ficam aqui */}
-        <Header/>
+      <Header />
+      <Footer/>
+      {/* <Routes>
+        Rotas publicas ficam aqui
         <Route
           path="/create-plant"
           element={
@@ -33,11 +34,11 @@ function App() {
           path="/user-config"
           element={
             <PrivateRoute>
-              <UserConfigPage/>
+              <UserConfigPage />
             </PrivateRoute>
           }
         />
-      </Routes>
+      </Routes> */}
     </>
   );
 }
--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -2,30 +2,42 @@
 
 const Footer = () => {
   return (
-    <div className="bg-emerald-950 bg-[url(./assets/images/fundo-footer.svg)] w-full h-[422px] pt-[80px] px-[83px] text-white">
+    <div className="bg-emerald-950 bg-[url(./assets/images/fundo-footer.svg)] w-full h-[422px] pt-[80px] pb-[48px] px-[83px] font-primary text-white">
       <div className="flex justify-between">
         <div>
-          <h2>Stay Fresh</h2>
-          <p>compassinhos@gmail.com</p>
-          <p>+55 41 99999-9999</p>
+          <h2 className="font-secondary text-[40px] font-bold">Stay Fresh</h2>
+          <p className="font-normal text-[16px] mt-4">compassinhos@gmail.com</p>
+          <p className="font-normal text-[16px] mt-4">+55 41 99999-9999</p>
         </div>
         <div className="flex gap-[95px] justify-between ">
           <div>
-            <h4>Links</h4>
-            <a href="#">About us</a>
+            <h3 className="font-bold text-2xl mb-8">Links</h3>
+            <ul>
+              <li>
+                <a href="#">About us</a>
+              </li>
+            </ul>
             {/* <Link to="/aboult-us"></Link> */}
           </div>
           <div>
-            <h4>Community</h4>
-            <p>LinkedIn</p>
-            <p>Instagran</p>
-            <p>Facebook</p>
+            <h3 className="font-bold text-2xl mb-8">Community</h3>
+            <ul>
+              <li className="pb-4 font-normal">
+                <a href="https://www.linkedin.com/login/pt" target="_blank">LinkedIn</a>
+              </li>
+              <li className="pb-4 font-normal">
+                <a href="https://www.instagram.com/" target="_blank">Instagran</a>
+              </li>
+              <li className="font-normal">
+                <a href="https://pt-br.facebook.com/" target="_blank">Facebook</a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
-      <div className="mt-[95px]  flex justify-between border-t-emerald-950 border-t-1 pt-[28px]">
+      <div className="mt-[48px] place-items-center flex justify-between border-t-emerald-950 border-t-1 pt-[28px]">
         <div className=" bg-[url('./assets/images/GroupPlant.png')] w-[49px] h-[54px]"></div>
-        <div>
+        <div className="font-primary">
           <p>Compassinhos ®. All rights reserved.</p>
         </div>
       </div>

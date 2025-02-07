27a5c48749c5a9248340be8882ef17c78b28a1b1
GBarela <g2004barela@hotmail.com>
2025-02-07 14:48:22 -0300

    chore: First commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> fc31645f171024ec79a283d8f88255982595d8cb
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'

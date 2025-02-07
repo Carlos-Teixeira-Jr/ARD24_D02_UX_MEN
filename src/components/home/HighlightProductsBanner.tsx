d0252ab032a964bf84f7669cfd5817a2febe8951
Gian Vieceli <gianvieceli@gmail.com>
2025-02-07 03:48:19 -0300

    chore: correçoes e solicitação de merge pra dev

--- src/components/home/HighlightProductsBanner.tsx
+++ src/components/home/HighlightProductsBanner.tsx
@@ -2,7 +2,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useEffect, useRef, useState } from "react";
-import { IFormDataPayload } from "../interfaces/CreatePlantInterface";
+import { IFormDataPayload } from "../../interfaces/CreatePlantInterface";
 
 const HighlightProductsBanner = () => {
   const [product, setProduct] = useState<IFormDataPayload[]>([]);

48588a54a5cf5c958def66c672a3a407c1fa091e
teixeirahist1988@gmail.com <pavitechjbr@gmail.com>
2025-02-08 14:56:16 -0300

    feat page implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Home from "./pages/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import { EditProductPage } from "./pages/edit";
 import { RegisterProductPage } from "./pages/register";
 import { UserConfigPage } from "./pages/userConfig";
@@ -9,14 +9,42 @@ import { AboutUsPage } from "./pages/aboutUs";
 import LoginForm from "./pages/loginForm";
 import { ListProductsPage } from "./pages/listProducts";
 import { ProductDetailsPage } from "./pages/productDetails";
+import { PageNotFoundPage } from "./pages/404Page";
+import { useEffect } from "react";
+
+const validRoutes = [
+  "/",
+  "/loginForm",
+  "/about-us",
+  "/page-not-found",
+  "products",
+  "products/new",
+  "products/:id/edit",
+  "user-config",
+];
+
+function RouteValidator() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!validRoutes.includes(location.pathname)) {
+      navigate("/page-not-found");
+    }
+  },[location.pathname, navigate]);
+
+  return null;
+}
 
 function App() {
   return (
     <>
+    <RouteValidator/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/loginForm" element={<LoginForm />} />
         <Route path="/about-us" element={<AboutUsPage />} />
+        <Route path="/page-not-found" element={<PageNotFoundPage />} />
         <Route
           path="/products"
           element={

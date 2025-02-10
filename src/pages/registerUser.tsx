bbf9a30bc9464c545d0f79589552638590d7584c
GBarela <g2004barela@hotmail.com>
2025-02-10 16:53:26 -0300

    chore: Register

--- src/pages/registerUser.tsx
+++ src/pages/registerUser.tsx
@@ -4,50 +4,66 @@ import { useSignIn, useUser } from "@clerk/clerk-react";
 import { FormEvent, useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
+import { useSignUp } from "@clerk/clerk-react";
+import { Link } from "react-router";
+
 const LoginForm: React.FC = () => {
 
     //REGISTER AINDA NÃO FUNCIONAL
   const navigate = useNavigate();
 
-  const { signIn, setActive } = useSignIn();
-  const { isSignedIn } = useUser();
+  const { signUp } = useSignUp();
 
+  const [firstName, setFirstName] = useState("");
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
-  const [confirm, setConfirmPassword] = useState("");
-
-  const [loading, setLoading] = useState(false);
 
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+
+  const { isSignedIn } = useUser();
+  const [confirmPS, setConfirmPassword] = useState("");
+
+  const clearFields = useCallback(() => {
+    setFirstName("");
+    setEmailAddress("");
+    setPassword("");
+  }, []);
+
 
   const handleSubmit = useCallback(
     async (event: FormEvent) => {
       event.preventDefault();
-
       setError("");
       setLoading(true);
 
       try {
-        const signInResource = await signIn?.create({
-          identifier: emailAddress,
-          password,
-        });
-
-        await setActive?.({ session: signInResource?.createdSessionId });
-        navigate("/");
-
+        await signUp?.create({ emailAddress, firstName, password });
       } catch (error: any) {
-        if (error?.errors?.[0]?.code === "session_exists") {
-          navigate("/");
-        } else {
-          setError(JSON.stringify(error?.errors));
-        }
-      } finally {
+        setError(JSON.stringify(error?.errors));
         setLoading(false);
+        return;
       }
+      try {
+        await signUp?.prepareVerification({
+          redirectUrl: "http://localhost:5173/verify",
+          strategy: "email_link",
+        });
+        setSuccess(true);
+        clearFields();
+      } catch (error: any) {
+        setError(JSON.stringify(error?.errors));
+      }
+      setLoading(false);
     },
-    [signIn, emailAddress, navigate, setActive, password]
+    [signUp, emailAddress, firstName, password, clearFields]
   );
+
+  if (success) {
+    return <h2>Check your email for the verification link</h2>;
+  }
 
   useEffect(() => {
     if (isSignedIn) {
@@ -70,12 +86,12 @@ const LoginForm: React.FC = () => {
           </p>
           <form onSubmit={handleSubmit}>
           <div className="mb-4">
-              <label htmlFor="name" className="block text-gray-700">Name:</label>
+              <label htmlFor="firstName" className="block text-gray-700">Name:</label>
               <input 
-                type="email"
-                id="emailAddress"
-                value={emailAddress}
-                onChange={(e) => setEmailAddress(e.target.value)}
+                type="text"
+                id="firstName"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
                 className="w-full p-2 border border-gray-300 rounded-lg mt-1"
                 placeholder="email@example.com"
                 required
@@ -106,11 +122,11 @@ const LoginForm: React.FC = () => {
               />
             </div>
             <div className="mb-4">
-              <label htmlFor="confirm" className="block text-gray-700">Confirm Password:</label>
+              <label htmlFor="confirmPS" className="block text-gray-700">Confirm Password:</label>
               <input
-                type="confirm"
-                id="confirm"
-                value={password}
+                type="confirmPS"
+                id="confirmPS"
+                value={confirmPS}
                 onChange={(e) => setConfirmPassword(e.target.value)}
                 className="w-full p-2 border border-gray-300 rounded-lg mt-1"
                 placeholder="••••••••"
@@ -118,7 +134,7 @@ const LoginForm: React.FC = () => {
               />
             </div>
             <button disabled={loading} type="submit" className="w-full bg-primary text-white p-3 rounded-lg mb-4 font-inter font-semibold cursor-pointer">Login
-                {loading ? "loading..." : "Sign in"}
+                {loading ? "loading..." : "Sign up"}
             </button>
             {error && <div>{error}</div>}
           </form>

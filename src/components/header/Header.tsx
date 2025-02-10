a846b6f5eead731318b7baa5ce86b36cf60901cb
Gian Vieceli <gianvieceli@gmail.com>
2025-02-10 14:03:08 -0300

    chore: clerk in Links

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import { DarkModeToggle } from "../darkMode/darkModeToggle";
 import { Link, NavLink } from "react-router-dom";
+import { useUser } from "@clerk/clerk-react";
+import { useClerk } from "@clerk/clerk-react";
 
 const Header = () => {
-  const [isSignedIn, setIsSignedIn] = useState<boolean>(true);
+  const isSignedIn = useUser().isSignedIn;;
+  const { signOut } = useClerk();
+ 
 
   return (
     <div className="flex place-items-center justify-between mx-auto px-[5px] md:px-[40px] h-[83px] border-slate-200 border-b-[1px]">
@@ -14,19 +18,55 @@ const Header = () => {
         {!isSignedIn ? (
           <ul>
             <li className="font-inter text-[16px] hover:text-[20px]">
-              <NavLink to="/"className={({ isActive }) => (isActive ? "text-emerald-900 hover:text-emeral-400" : "text-slate-500 hover:text-slate-900")}>Home</NavLink>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-emerald-900 hover:text-emeral-400"
+                    : "text-slate-500 hover:text-slate-900"
+                }
+              >
+                Home
+              </NavLink>
             </li>
           </ul>
         ) : (
           <ul className="flex relative md:gap-[16px] gap-[10px] md:p-[16px]">
             <li className="font-inter text-[16px] hover:text-[20px]">
-              <NavLink to="/"className={({ isActive }) => (isActive ? "text-emerald-900 hover:text-emeral-400" : "text-slate-500 hover:text-slate-900")}>Home</NavLink>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-emerald-900 hover:text-emeral-400"
+                    : "text-slate-500 hover:text-slate-900"
+                }
+              >
+                Home
+              </NavLink>
             </li>
             <li className="font-inter text-[16px] hover:text-[20px]">
-              <NavLink to="/catalogo"className={({ isActive }) => (isActive ? "text-emerald-900 hover:text-emeral-700" : "text-slate-500 hover:text-slate-900")}>Poducts</NavLink>
+              <NavLink
+                to="/catalogo"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-emerald-900 hover:text-emeral-700"
+                    : "text-slate-500 hover:text-slate-900"
+                }
+              >
+                Poducts
+              </NavLink>
             </li>
             <li className="font-inter text-[16px] hover:text-[20px]">
-              <NavLink to="/user-config"className={({ isActive }) => (isActive ? "text-emerald-900 hover:text-emeral-700" : "text-slate-500 hover:text-slate-900")}>About me</NavLink>
+              <NavLink
+                to="/user-config"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-emerald-900 hover:text-emeral-700"
+                    : "text-slate-500 hover:text-slate-900"
+                }
+              >
+                About me
+              </NavLink>
             </li>
           </ul>
         )}
@@ -35,12 +75,13 @@ const Header = () => {
         {isSignedIn ? (
           <div className="flex gap-4">
             <DarkModeToggle />
-            <Link
-              onClick={() => setIsSignedIn(false)}
-              to="/"
-              className="md:px-[40px] md:py-[12px] py-2 px-2 cursor-pointer bg-emerald-900 hover:bg-emerald-700 rounded-[8px] text-white">
-              Log out
-            </Link>
+              <Link
+                onClick={signOut}
+                to="/"
+                className="md:px-[40px] md:py-[12px] py-2 px-2 cursor-pointer bg-emerald-900 hover:bg-emerald-700 rounded-[8px] text-white"
+              >
+                Log out
+              </Link>
           </div>
         ) : (
           <ul className="font-[inter] text-[16px] flex gap-[40px]">
